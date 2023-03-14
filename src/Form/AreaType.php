cde1a384f9bf986386d1dfb126b1cf83899f3920
Christian <chrisjav28@hotmail.com>
2023-03-13 23:01:08 -0300

    mejor templates

--- src/Form/AreaType.php
+++ src/Form/AreaType.php
@@ -6,14 +6,20 @@ use App\Entity\Area;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AreaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre')
-            ->add('descripcion')
+            ->add('nombre', TextType::class, [
+            'attr' => ['class' => 'form-control'],
+             ])
+
+             ->add('descripcion', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
         ;
     }
 
